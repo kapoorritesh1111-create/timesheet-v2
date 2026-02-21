f510a88ddca77e1291d24af6f4f62ff5a38ba058
kapoorritesh1111-create <kapoorritesh1111@gmail.com>
2026-02-21 18:22:41 -0500

    Update src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import "./globals.css";
-
-export const metadata = { title: "Timesheet Webapp" };
+import ThemeProvider from "../components/theme/ThemeProvider";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
